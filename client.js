e53bd2586ea28e8ee02fc0f2b20015a0990aaa03
6210110459 <6210110459@psu.ac.th>
2022-01-24 14:58:14 +0700

    part1

---
+++ client.js
@@ -0,0 +1,28 @@
+var net = request('net');
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+var client = new net.socket();
+client.connect(PORT, HOST, function() {
+    console.log('CONNECTEF TO: ' + HOST + ':' +PORT);
+    readline.question('\n', input => {
+        client.write(input);
+    })   
+});
+
+client.on('data', function(data) {
+    readline.question(data  + '\n', input => {
+        client.write(input);
+    })
+});
+
+client.on('close',function() {
+    console.log('Connection closeed');
+})
